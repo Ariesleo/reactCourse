c6e7adca2f19adf64f9142fad378c73d7ebd10c4
Murari Gupta <murarigupta2367@gmail.com>
2021-03-22 14:56:40 +0545

    created a form in new component called SearchParams

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="kaley" animal="dog" breed="havaness" />
+      <SearchParams />
     </div>
   );
 };
