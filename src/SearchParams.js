544f9ef90faf5a9c81873f9af1df4b197dd955a7
Murari Gupta <murarigupta2367@gmail.com>
2021-03-23 12:22:10 +0545

    adding custom hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
-  const [animal, updateAnimal] = useState("bird");
+  const [animal, updateAnimal] = useState("");
   const [breed, updateBreed] = useState("");
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    console.table(json);
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
@@ -56,6 +73,14 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
